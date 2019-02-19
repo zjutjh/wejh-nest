abd9f52d33c07a9594d5d3c13ef39852f74e5dd9
cccRaim <chen.raim@qq.com>
2019-02-19 15:11:49 +0800

    add husky

--- src/service/user.center.service.ts
+++ src/service/user.center.service.ts
@@ -1,6 +1,7 @@
 import * as request from 'request-promise-native';
 import {getApiUrl} from "../library/helper";
 import {Injectable} from "@nestjs/common";
+
 @Injectable()
 export class UserCenterService {
     async checkJHPassport(username: string, password: string) {
